464d1025b2041b6b2452c8667717f7f5c72c7119
Takemi Watanuki <takemi007@hotmail.com>
2015-03-21 01:18:54 +0900

    +++class

---
+++ LiquidCall/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  LiquidCall
+//
+//  Created by Takemi Watanuki on 2015/03/21.
+//  Copyright (c) 2015年 watakemi725. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
