77ceec38fcde190ff9b99485ab945dc2c51b58bc
Takemi Watanuki <watakemi725@Takemi-no-MacBook-Pro.local>
2015-03-20 21:51:15 +0900

    hello

---
+++ LiquidCallTests/LiquidCallTests.swift
@@ -0,0 +1,36 @@
+//
+//  LiquidCallTests.swift
+//  LiquidCallTests
+//
+//  Created by Takemi Watanuki on 2015/03/20.
+//  Copyright (c) 2015年 watakemi725. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LiquidCallTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
